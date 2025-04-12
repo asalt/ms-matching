2fffabf78ff4be87febc9089114bd57546b18122
Alex Saltzman <saltzman@bcm.edu>
2025-04-11 20:13:12 -0500

    digest with modifications

--- src/ms_matching/digest.py
+++ src/ms_matching/digest.py
@@ -1,6 +1,59 @@
+# digest.py
+from itertools import combinations
 from pyteomics import parser
+from pyteomics import mass
+from ms_matching.io import read_fasta
+from ms_matching import match
 
-# example
+import numpy as np
+
+
+default_tag_map = {
+    15.995: "ox",
+    79.966: "ph",
+    42.011: "ac",
+}
+
+
+def mz_to_bin(mz_array, bin_width):  # TODO let it take ppm
+    # TODO add check if not np.array
+    return np.round((mz_array - bin_width / 2) / bin_width).astype(int)
+
+
+def build_aa_mass_from_variable_mods(config, tag_map=default_tag_map):
+    """
+    Parses MSFragger-style variable_mod entries into a pyteomics-compatible aa_mass dict.
+
+    Args:
+        config: dict from parse_msfragger_config()
+        tag_map: optional map from rounded delta mass to prefix tag (e.g. 15.995 → 'ox')
+
+    Returns:
+        aa_mass: modified dict for pyteomics
+        mod_aliases: mapping from base AA to modified tag name
+    """
+    aa_mass = dict(mass.std_aa_mass)
+    mod_aliases = {}
+
+    keys = [x for x in config.keys() if x.startswith("variable_mod_")]
+    for key in keys:
+        value = config[key]
+        parts = value.split()
+        if len(parts) < 2:
+            continue
+        delta_mass = float(parts[0])
+        targets = parts[1]
+        rounded_mass = round(delta_mass, 3)
+
+        tag_prefix = tag_map.get(rounded_mass, f"mod{int(abs(delta_mass) * 1000)}")
+        for aa in targets:
+            if aa in mass.std_aa_mass:
+                tag = f"{tag_prefix}{aa}"
+                aa_mass[tag] = mass.std_aa_mass[aa] + delta_mass
+                mod_aliases.setdefault(aa, []).append(tag)
+            else:
+                pass  # could log/handle N-term/C-term cases separately later
+    return aa_mass, mod_aliases
 
 
 def digest_protein(
@@ -43,11 +96,87 @@ def digest_protein(
         max_length=max_length,
     )
 
-    assert len(peptides) > 0, "No peptides were generated from the protein sequence."
-
+    # assert len(peptides) > 0, "No peptides were generated from the protein sequence."
 
     return peptides
 
+
+def generate_mod_variants(
+    peptide: str, mod_aliases: dict, max_mods_per_peptide: int = 3
+):
+    """
+    Generate all variants of a peptide with up to N modifications.
+
+    Args:
+        peptide: e.g. "STYMQL"
+        mod_aliases: e.g. {'S': ['phS'], 'T': ['phT'], 'M': ['oxM']}
+        max_mods_per_peptide: maximum allowed modifications of each type
+
+    Returns:
+        List of parsed peptide sequences suitable for fast_mass2
+    """
+    variants = set()
+
+    # Get all modifiable positions and the mod tag to apply
+    mod_sites = [
+        (i, mod_aliases[aa][0]) for i, aa in enumerate(peptide) if aa in mod_aliases
+    ]
+
+    for n in range(1, max_mods_per_peptide + 1):
+        for mod_pos_combo in combinations(mod_sites, n):
+            mod_seq = list(peptide)
+            for i, mod_tag in mod_pos_combo:
+                mod_seq[i] = mod_tag
+            variants.add(tuple(mod_seq))  # Use tuple to deduplicate
+    return [parser.parse("".join(seq)) for seq in variants]
+
+
+def generate_peptides(protein="AAAQQQQSMETK", **kwargs):
+
+    proteins = kwargs.get("proteins", ["AAAQQQQSMETK", "AAAQQQCPALK"])
+    max_modis = kwargs.get("max_modis", 3)
+    config = kwargs.get(
+        "config",
+        {
+            "variable_mod_01": "15.9949 M 3",
+            # 'variable_mod_02': '42.0106 [^ 1',
+            "variable_mod_03": "79.96633 STY 3",
+        },
+    )
+    aa_mass, mod_aliases = build_aa_mass_from_variable_mods(
+        config, tag_map=default_tag_map
+    )
+    peptides = list()  # we will not do this later
+    # for header, aa_seq in proteins:
+    digest = digest_protein(protein)  # TODO add more args here
+    for peptide in digest:
+        peptide_modis = generate_mod_variants(
+            peptide, mod_aliases, max_mods_per_peptide=max_modis
+        )
+        peptides.append(peptide)
+        peptides += peptide_modis
+    return peptides, aa_mass
+
+
+def generate_theoretical_fragments(fasta_file, config=None, bin_width=0.5):
+
+    if config is None:
+        config = {
+            "variable_mod_01": "15.9949 M 3",
+            # 'variable_mod_02': '42.0106 [^ 1',
+            "variable_mod_03": "79.96633 STY 3",
+        }
+
+    for header, aaseq in read_fasta(fasta_file):
+        peptides, aa_masses = generate_peptides(aaseq, config=config)
+        for peptide in peptides:
+            frags = match.make_fragments(peptide, aa_mass=aa_masses)  # generator
+            frags = mz_to_bin(np.array(list(frags)), bin_width)
+
+            _peptide = peptide
+            if isinstance(_peptide, list):
+                _peptide = "".join(_peptide)
+            yield _peptide, frags
 
 
 #
--- src/ms_matching/match.py
+++ src/ms_matching/match.py
@@ -2,23 +2,34 @@ from pyteomics import parser, mass
 
 
 # we can also use pyteomics.mass.fast_mass to perform all of these calculations for us
-def make_fragments(peptide, types=('b', 'y'), maxcharge=1): # from https://pyteomics.readthedocs.io/en/latest/examples/example_msms.html
+def make_fragments(
+    peptide, types=("b", "y"), maxcharge=1, aa_mass=mass.std_aa_mass
+):  # from https://pyteomics.readthedocs.io/en/latest/examples/example_msms.html
     """
     The function generates all possible m/z for fragments of types
     `types` and of charges from 1 to `maxharge`.
     """
     for i in range(1, len(peptide)):
         for ion_type in types:
-            for charge in range(1, maxcharge+1):
-                if ion_type[0] in 'abc':
-                    yield mass.fast_mass(
-                            peptide[:i], ion_type=ion_type, charge=charge)
+            for charge in range(1, maxcharge + 1):
+                if ion_type[0] in "abc":
+                    yield mass.fast_mass2(
+                        peptide[:i], ion_type=ion_type, charge=charge, aa_mass=aa_mass
+                    )
                 else:
-                    yield mass.fast_mass(
-                            peptide[i:], ion_type=ion_type, charge=charge)
+                    yield mass.fast_mass2(
+                        peptide[i:], ion_type=ion_type, charge=charge, aa_mass=aa_mass
+                    )
 
 
-def do_match(peptide_sequence, raw_mz, error_tol=20, error_tol_units="ppm", verbose=True, **kwargs):
+def do_match(
+    peptide_sequence,
+    raw_mz,
+    error_tol=20,
+    error_tol_units="ppm",
+    verbose=True,
+    **kwargs,
+):
     """
     Match the peptide sequence to the raw m/z data.
     Parameters
@@ -38,27 +49,36 @@ def do_match(peptide_sequence, raw_mz, error_tol=20, error_tol_units="ppm", verb
     if error_tol_units not in ("ppm", "Da"):
         raise ValueError(f"error_tol_units must be one of ('ppm', 'Da')")
     if error_tol_units == "ppm":
-        error_tol = error_tol * 1e-6 * raw_mz["m/z array"].mean() # convert ppm to Da, approximating the mean m/z as the average observed value
-
-
+        error_tol = (
+            error_tol * 1e-6 * raw_mz["m/z array"].mean()
+        )  # convert ppm to Da, approximating the mean m/z as the average observed value
 
     params = raw_mz["params"]
-    parent_ion_charge = int(params['charge'][0])
+    parent_ion_charge = int(params["charge"][0])
     mz_array = raw_mz["m/z array"]
     intensity_array = raw_mz["intensity array"]
 
-    b_fragments = list(make_fragments(peptide_sequence, types='b', maxcharge=parent_ion_charge-1))
-    y_fragments = list(make_fragments(peptide_sequence, types='y', maxcharge=parent_ion_charge-1))
+    b_fragments = list(
+        make_fragments(peptide_sequence, types="b", maxcharge=parent_ion_charge - 1)
+    )
+    y_fragments = list(
+        make_fragments(peptide_sequence, types="y", maxcharge=parent_ion_charge - 1)
+    )
 
     # how does this work?
     # could be rewritten to be more clear
-    b_matches = [ x for x in b_fragments if any( abs( x - y ) < error_tol for y in mz_array ) ]
-    y_matches = [ x for x in y_fragments if any( abs( x - y ) < error_tol for y in mz_array ) ]
+    b_matches = [
+        x for x in b_fragments if any(abs(x - y) < error_tol for y in mz_array)
+    ]
+    y_matches = [
+        x for x in y_fragments if any(abs(x - y) < error_tol for y in mz_array)
+    ]
     # how do we get the intensities for the matched fragments?
 
-
     # solution
-    assert len(mz_array) == len(intensity_array), "m/z array and intensity array must be the same length"
+    assert len(mz_array) == len(
+        intensity_array
+    ), "m/z array and intensity array must be the same length"
     # we can assume this will always be true, but we can check it anyway
 
     b_fragment_intensities = []
@@ -68,16 +88,18 @@ def do_match(peptide_sequence, raw_mz, error_tol=20, error_tol_units="ppm", verb
                 if verbose:
                     print(f"Match found for b fragment {b_fragment} at m/z {mz}")
                 b_fragment_intensities.append(intensity)
-                break # break because we have found the match
+                break  # break because we have found the match
 
     y_fragment_intensities = []
     for y_fragment in y_fragments:
         for mz, intensity in zip(mz_array, intensity_array):
             if abs(y_fragment - mz) < error_tol:
                 if verbose:
-                    print(f"Match found for y fragment {y_fragment} at m/z {mz} with intensity {intensity}")
+                    print(
+                        f"Match found for y fragment {y_fragment} at m/z {mz} with intensity {intensity}"
+                    )
                 b_fragment_intensities.append(intensity)
-                break # break because we have found the match
+                break  # break because we have found the match
 
     total_intensity = sum(b_fragment_intensities) + sum(y_fragment_intensities)
     n_matches = len(b_fragment_intensities) + len(y_fragment_intensities)
